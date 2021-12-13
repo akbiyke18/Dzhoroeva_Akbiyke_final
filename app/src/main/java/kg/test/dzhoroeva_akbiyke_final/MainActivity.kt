68cc05ece9f1565bab132db896478abf6c8d4df2
админ <dzhoroeva_a@auca,kg>
2021-12-13 14:59:05 +0600

    Final

---
+++ app/src/main/java/kg/test/dzhoroeva_akbiyke_final/MainActivity.kt
@@ -0,0 +1,22 @@
+package kg.test.dzhoroeva_akbiyke_final
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    val imageView =findViewById<ImageView>(R.id.imageView6)
+
+    imageView.setOnClickListener {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+
+
+}
